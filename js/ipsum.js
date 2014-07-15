0184d15b9c85fb2d222cc668be11b5ab3e6ea6dc
Terence Devine <terence@ligcreative.com>
2014-07-15 16:29:32 -0600

    add bluth ipsum function file, make universal for multiple use cases

---
+++ js/ipsum.js
@@ -0,0 +1,22 @@
+var textNum = [[doc askForUserInput: "How Many? Her?" initialValue: "1"] integerValue],
+	bluthIpsum = function(num, type) {
+
+	var textDiv = (type === "sentence") ? '. ' : ' '
+
+	for (var i = 0; selection.length; i++) {
+		var layer = selection[i]
+
+		if([layer class] === MSTextLayer) {
+			var ipsumNum = Math.floor(Math.random() * data.length),
+				ipsumStr = data[ipsumNum].split(textDiv, num).join(textDiv) + textDiv,
+				dataRemove = data.splice(ipsumNum, 1)
+		
+			[layer setStringValue: ipsumStr]
+			[layer setName: ipsumStr]
+			[layer adjustFrameToFit]
+
+		} else {
+			[[NSApplication sharedApplication] displayDialog: "This isn't a text field...  Ain't nobody got time for that!" withTitle: "Her?"]
+		}
+	}
+}
